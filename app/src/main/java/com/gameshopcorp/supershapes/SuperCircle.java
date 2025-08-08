a2d8cfd85f31ecba88039bcc2abdcedf29af1736
Lynden Jay Evans <gameshopengine@gmail.com>
2025-08-08 09:12:18 -0400

    Making SuperCircle.  Added Degree Vectors

--- app/src/main/java/com/gameshopcorp/supershapes/SuperCircle.java
+++ app/src/main/java/com/gameshopcorp/supershapes/SuperCircle.java
@@ -2,16 +2,39 @@ package com.gameshopcorp.supershapes;
 
 import com.jme3.math.Vector3f;
 
+import java.util.HashMap;
+
 public class SuperCircle {
 
     public Vector3f origin;
     public Vector3f anchor;
-    public SuperCircle(Vector3f origin, Vector3f anchor){
+    public byte numPoints;
+    public HashMap<Float, Vector3f> degreeVectors;
+    public SuperCircle(Vector3f origin, Vector3f anchor, byte numPoints){
 
         this.origin = origin;
         this.anchor = anchor;
+        this.numPoints = numPoints;
+        this.degreeVectors = new HashMap<>();
+    }
+
+    public void make(){
+
+        for (int i = 0; i < numPoints; i++){
+
+        }
+    }
+
+    public void cull(){
 
     }
+
+    public void render(){
+
+    }
+
+
+
 
 
 }

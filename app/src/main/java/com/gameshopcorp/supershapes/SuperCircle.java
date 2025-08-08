31d3dc396339c90e8b5bbf8a66765516464101b3
Lynden Jay Evans <gameshopengine@gmail.com>
2025-08-08 08:54:40 -0400

    Making SuperCircle

---
+++ app/src/main/java/com/gameshopcorp/supershapes/SuperCircle.java
@@ -0,0 +1,13 @@
+package com.gameshopcorp.supershapes;
+
+import com.jme3.math.Vector3f;
+
+public class SuperCircle {
+
+    public Vector3f origin;
+    public Vector3f anchor;
+    public SuperCircle(Vector3f origin, Vector3f anchor){
+
+
+    }
+}
